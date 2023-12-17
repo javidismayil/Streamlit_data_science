8d42330da6ff6287453fe53459bf5f7660a72063
javidismayil <cavid.ismayilov97@gmail.com>
2023-12-17 21:27:19 +0400

    4

--- project.py
+++ project.py
@@ -10,9 +10,9 @@ import numpy as np
 from PIL import Image
 import seaborn as sns
 import matplotlib.pyplot as plt
-icon = Image.open(r'D:\JAVID_ISMAYILOV_PROF\MB\QSS\python\week8\myproject\1702665115899.jpg')
-banner = Image.open(r'D:\JAVID_ISMAYILOV_PROF\MB\QSS\python\week8\myproject\1702664939130.jpg')
-sidebar = Image.open(r'D:\JAVID_ISMAYILOV_PROF\MB\QSS\python\week8\myproject\1702665115899.jpg')
+icon = Image.open(r'C:\Users\PC\Documents\GitHub\Streamlit_data_science\1702665115899.jpg')
+banner = Image.open(r'C:\Users\PC\Documents\GitHub\Streamlit_data_science\1702664939130.jpg')
+sidebar = Image.open(r'C:\Users\PC\Documents\GitHub\Streamlit_data_science\1702665115899.jpg')
 
 
 #st.set_page_config(page_title = 'AnalyticaView', page_icon = icon, layout = 'wide')
